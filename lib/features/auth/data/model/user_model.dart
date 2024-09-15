e8c7e50c8bbcd90b7794bc29e2fb859be71ea643
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-09-15 14:17:51 +0300

    Added some fields

--- lib/features/auth/data/model/user_model.dart
+++ lib/features/auth/data/model/user_model.dart
@@ -3,15 +3,27 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../domain/entities/user.dart';
 
 class UserModel extends User {
-  const UserModel({required super.name, required super.id});
+  UserModel({
+    required super.name,
+    required super.id,
+    super.address,
+    super.age,
+    super.phone,
+  });
 
   Map<String, dynamic> toMap() => {
     'name': name,
+    'address': address,
+    'age' : age,
+    'phone' : phone,
   };
 
   factory UserModel.fromDoc(QueryDocumentSnapshot<Map<String, dynamic>> doc) {
     return UserModel(
       name: doc.data()['name'],
+      address: doc.data()['address'],
+      age: doc.data()['age'],
+      phone: doc.data()['phone'],
       id: doc.id,
     );
   }
--- lib/features/e_commerce/presentation/pages/contact_page.dart
+++ lib/features/e_commerce/presentation/pages/contact_page.dart
@@ -1,14 +1,22 @@
 import 'package:e_commerce_app/features/auth/data/model/user_model.dart';
 import 'package:e_commerce_app/features/auth/presentation/bloc/auth_bloc/authentication_bloc.dart';
-import 'package:e_commerce_app/features/auth/presentation/bloc/user_data_bloc/user_data_bloc.dart';
-import 'package:e_commerce_app/features/auth/presentation/pages/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app/core/app_theme.dart';
 
-class ContactPage extends StatelessWidget {
-  ContactPage({super.key});
+class ContactPage extends StatefulWidget{
+  const ContactPage({super.key});
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+
+  bool editable = false;
+  Icon editIcon = const Icon(Icons.edit);
+  String editToolTip = "Edit Info";
 
   @override
   Widget build(BuildContext context) {    
@@ -20,29 +28,50 @@ class ContactPage extends StatelessWidget {
           builder: (context, snapshot){
             // var userModel = snapshot.data?.firstWhere((element) => element.id == FirebaseAuth.instance.currentUser!.uid);
             final user = FirebaseAuth.instance.currentUser;
-            var globalKeyForm = GlobalKey<FormState>();
-            var userModel = UserModel(name: "Omar",id: "123");
+            final globalKeyForm = GlobalKey<FormState>();
+            final userModel = UserModel(name: "Omar",id: "123");
             final nameController = TextEditingController(text: userModel?.name);
             final emailController = TextEditingController(text: /*"${user?.email}"*/"Omar@anything.com");
             final oldPasswordController = TextEditingController();
             final newPasswordController = TextEditingController();
-            final confirmNewPasswordController = TextEditingController();
+            final confirmPasswordController = TextEditingController();
+            final addressController = TextEditingController(text: userModel?.address);
+            final ageController = TextEditingController(text: userModel?.age?.toString());
+            final phoneController = TextEditingController(text: userModel?.phone);
+
+
             return userModel != null? SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Welcome, ${userModel.name}",style: appTheme.textTheme.bodyLarge),
-                  Text(/*"${user?.email}"*/"Omar@anything.com", style: appTheme.textTheme.bodyMedium),
+                  Row(
+                    children: [
+                      Text("Welcome, ${userModel.name}",style: appTheme.textTheme.bodyLarge),
+                      const Spacer(),
+                      IconButton(
+                        icon: editIcon,
+                        onPressed: (){
+                          setState((){
+                            editable = !editable;
+                            editIcon = editable? const Icon(Icons.cancel_outlined):const Icon(Icons.edit);
+                            editToolTip = editable? "Cancel":"Edit Info";
+                          });
+                        },
+                        tooltip: editToolTip,
+                      ),
+                    ],
+                  ),
                   const SizedBox(height: 25.0,),
                   Form(
                     key: globalKeyForm,
                     child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Your Full Name",style: appTheme.textTheme.bodyLarge),
                         TextFormField(
                           controller: nameController,
+                          enabled: false,
                           decoration: InputDecoration(
+                            label: Text("Full Name",style: appTheme.textTheme.bodyMedium,),
                             floatingLabelStyle: TextStyle(color: primaryColor),
                             iconColor: secondaryColor,
                             focusedBorder: OutlineInputBorder(
@@ -55,13 +84,15 @@ class ContactPage extends StatelessWidget {
                             ),
                           ),
                         ),
+
                         const SizedBox(height: 25.0,),
-                        Text("Your Email",style: appTheme.textTheme.bodyLarge),
 
                         TextFormField(
                           controller: emailController,
-                          readOnly: true,
+                          // readOnly: true,
+                          enabled: false,
                           decoration: InputDecoration(
+                            label: Text("Email",style: appTheme.textTheme.bodyMedium,),
                             floatingLabelStyle: TextStyle(color: primaryColor),
                             iconColor: secondaryColor,
                             focusedBorder: OutlineInputBorder(
@@ -75,10 +106,85 @@ class ContactPage extends StatelessWidget {
                           ),
                         ),
                         const SizedBox(height: 25.0,),
-                        Text("Old Password",style: appTheme.textTheme.bodyLarge),
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child:TextFormField(
+                                keyboardType: TextInputType.number,
+                                controller: ageController,
+                                enabled: editable,
+                                decoration: InputDecoration(
+                                  label: Text("Age",style: appTheme.textTheme.bodyMedium,),
+                                  floatingLabelStyle: TextStyle(color: primaryColor),
+                                  iconColor: secondaryColor,
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: secondaryColor),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: primaryColor),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 10.0,),
+                            Expanded(
+                              flex: 7,
+                              child: TextFormField(
+                                keyboardType: TextInputType.phone,
+                                controller: phoneController,
+                                enabled: editable,
+                                decoration: InputDecoration(
+                                  label: Text("Phone Number",style: appTheme.textTheme.bodyMedium,),
+                                  floatingLabelStyle: TextStyle(color: primaryColor),
+                                  iconColor: secondaryColor,
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: secondaryColor),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: primaryColor),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                                validator: (text){
+                                  if (text!.isNotEmpty && text.length != 11) {
+                                    return 'phone number must be 11 digits';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 25.0,),
+                        TextFormField(
+                          controller: addressController,
+                          enabled: editable,
+                          decoration: InputDecoration(
+                            label: Text("Address",style: appTheme.textTheme.bodyMedium,),
+                            floatingLabelStyle: TextStyle(color: primaryColor),
+                            iconColor: secondaryColor,
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: secondaryColor),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: primaryColor),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ),
+                        
+                        const SizedBox(height: 25.0,),
+
                         TextFormField(
                           controller: oldPasswordController,
+                          enabled: editable,
                           decoration: InputDecoration(
+                            label: Text("Old Password",style: appTheme.textTheme.bodyMedium,),
                             floatingLabelStyle: TextStyle(color: primaryColor),
                             iconColor: secondaryColor,
                             focusedBorder: OutlineInputBorder(
@@ -92,7 +198,7 @@ class ContactPage extends StatelessWidget {
                           ),
                           validator: (text){
                             if (text == null || text.isEmpty){
-                              if (newPasswordController.text.isNotEmpty || confirmNewPasswordController.text.isNotEmpty){
+                              if (newPasswordController.text.isNotEmpty || confirmPasswordController.text.isNotEmpty){
                                 return 'You must enter your old password';
                               }
                               return null;
@@ -106,11 +212,14 @@ class ContactPage extends StatelessWidget {
                             }
                           },
                         ),
+                        
                         const SizedBox(height: 25.0,),
-                        Text("New Password",style: appTheme.textTheme.bodyLarge),
+
                         TextFormField(
                           controller: newPasswordController,
+                          enabled: editable,
                           decoration: InputDecoration(
+                            label: Text("New Password",style: appTheme.textTheme.bodyMedium,),
                             floatingLabelStyle: TextStyle(color: primaryColor),
                             iconColor: secondaryColor,
                             focusedBorder: OutlineInputBorder(
@@ -136,11 +245,14 @@ class ContactPage extends StatelessWidget {
                             }
                           },
                         ),
+                        
                         const SizedBox(height: 25.0,),
-                        Text("Confirm New Password",style: appTheme.textTheme.bodyLarge),
+
                         TextFormField(
-                          controller: confirmNewPasswordController,
+                          controller: confirmPasswordController,
+                          enabled: editable,
                           decoration: InputDecoration(
+                            label: Text("Confirm Password",style: appTheme.textTheme.bodyMedium,),
                             floatingLabelStyle: TextStyle(color: primaryColor),
                             iconColor: secondaryColor,
                             focusedBorder: OutlineInputBorder(
@@ -159,34 +271,54 @@ class ContactPage extends StatelessWidget {
                             return "Password doesn't match";
                           }
                         ),
+                        
                         const SizedBox(height: 25.0,),
-                        Center(
-                          child: ElevatedButton(
-                            onPressed: (){
-                              if (globalKeyForm.currentState!.validate()){
-                                user?.updatePassword(newPasswordController.text);
-                                print(user);
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text('Changes Saved Successfully'),
-                                    duration: Duration(seconds: 2),
-                                  ),
-                                );
-                              }
-                            },
-                            style: appTheme.elevatedButtonTheme.style,
-                            child: const Text("Save"),
+                        
+                        Visibility(
+                          visible: editable,
+                          child: Center(
+                            child: ElevatedButton(
+                              onPressed: (){
+                                if (globalKeyForm.currentState!.validate()){
+                                  if (newPasswordController.text.isNotEmpty){
+                                    user?.updatePassword(newPasswordController.text);
+                                  }
+                                  print(user);
+                                  if (ageController.text.isNotEmpty){
+                                    userModel.age = int.parse(ageController.text);
+                                  }
+                                  if (phoneController.text.isNotEmpty){
+                                    userModel.phone = phoneController.text;
+                                  }
+                                  if (addressController.text.isNotEmpty){
+                                    userModel.address = addressController.text;
+                                  }
+                                  print(userModel.age);
+                                  // update in firebase
+                                  // context.read<UserDataBloc>().add(SetUserEvent(userModel: userModel));
+                                  
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Changes Saved Successfully'),
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  );
+                                }
+                              },
+                              style: appTheme.elevatedButtonTheme.style,
+                              child: const Text("Save"),
+                            ),
                           ),
                         ),
                       ],
-                    )
-                  )
+                    ),
+                  ),
                 ],
               ),
             ):const Text("No Data");
-          }
+          },
         ),
-      )
+      ),
     );
   }
 }
